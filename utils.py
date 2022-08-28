3d3cee5da87305e38c79841b70a078533fc1dace
Ruilong Tang <trlongsama@gmail.com>
2022-08-28 16:30:32 -0400

    user-based collaborative filtering

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def data_load(file_path, headers):
-    df = pd.read_csv(file_path, sep='::', header=0, names=headers, engine='python')
-
-    return df
+def get_num_group(floor, ceiling, dataset, index_name):
+    group = dataset.apply(lambda x: True if ceiling > x[index_name] > floor else False, axis=1)
+    count = len(group[group == True].index)
+    return count
 
